efd1d331c2c2fd4b85a1711ddbad584ce1376f3e
planfittoflaunt-a11y <plan.fittoflaunt@gmail.com>
2025-10-25 17:59:57 +0530

    Update service worker to fetch resources from network
    Enhance service worker to respond with network fetch.

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 // sw.js: Minimal service worker required for PWA installability.
 self.addEventListener('fetch', function(event) {
   // This worker does not cache content, it simply allows the PWA check to pass.
+  // It responds by fetching the requested resource from the network.
+  event.respondWith(fetch(event.request));
 });
